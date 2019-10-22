85ffbd7f089864d1c28400021f92bb64a0a2267b
Максим Плющ <mplyushch@ivi.ru>
2019-10-22 16:30:51 +0300

    Fixes

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { TodoList } from './todoList/components/TodoList';
 import { TodoList as TodoListStore } from './todoList/store/TodoList';
-import { Todo } from './todoList/store/Todo';
 
 const store = new TodoListStore();
 
